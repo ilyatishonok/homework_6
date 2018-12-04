8d652eb2e386198b9571ec54f8a7ff23bbdba67b
Ilya <tishonok.ilya@ya.ru>
2018-12-04 23:42:13 +0300

    Updates

---
+++ precache-manifest.29481b1d3aa9ec8a9a23a88e715ce528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbb659c2d27ebc93ad42",
+    "url": "/homework_6/static/js/runtime~main.bbb659c2.js"
+  },
+  {
+    "revision": "ab3520e64a179e37362a",
+    "url": "/homework_6/static/js/main.ab3520e6.chunk.js"
+  },
+  {
+    "revision": "973c372bb24e079486e1",
+    "url": "/homework_6/static/js/1.973c372b.chunk.js"
+  },
+  {
+    "revision": "ab3520e64a179e37362a",
+    "url": "/homework_6/static/css/main.34b4808a.chunk.css"
+  },
+  {
+    "revision": "822ca8833cf5414a2d080b5e4d7f99d5",
+    "url": "/homework_6/index.html"
+  }
+];
